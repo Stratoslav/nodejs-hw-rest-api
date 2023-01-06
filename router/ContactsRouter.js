30a4b59304dc6c958d7523261ee33eba577912cc
Stratoslav <stas.kurbanov03@gmail.com>
2023-01-06 11:57:27 +0300

    add new file

--- router/ContactsRouter.js
+++ router/ContactsRouter.js
@@ -1,31 +1,20 @@
 const { Router } = require("express");
-const Contacts = require("../contacts");
+const {
+  getContactsController,
+  createContactsController,
+  updateContactController,
+  removeContactController,
+  favoriteContactsController,
+  getAllFavoriteContacts,
+} = require("../contacts/contacts.controller");
 
 const contactsRouter = Router();
 
-contactsRouter.get("/", async (req, res) => {
-  const contacts = await Contacts.listContacts();
-  res.json(contacts);
-  //OR WE CAN USE --->
-  //  res.setHeader("Content-Type", "application/json");
-  // res.send(JSON.stringify(contacts));
-});
+contactsRouter.get("/contacts", getContactsController);
+contactsRouter.get("/contacts/favorite", getAllFavoriteContacts);
+contactsRouter.post("/contacts", createContactsController);
+contactsRouter.patch("/contacts/:id", updateContactController);
+contactsRouter.delete("/contacts/:id", removeContactController);
+contactsRouter.patch("/contacts/:id/favorite", favoriteContactsController);
 
-contactsRouter.post("/", async (req, res) => {
-  const { name, email, phone } = req.body;
-
-  if (typeof name === "string" && typeof email === "string") {
-    const createContacts = await Contacts.addContact(name, email, phone);
-    res.json(createContacts);
-    res.status(201);
-  }
-  res.status(400).send("Invalid value");
-});
-
-contactsRouter.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  const deleteById = await Contacts.removeContact(+id);
-  res.json(deleteById);
-  res.end();
-});
 module.exports = contactsRouter;
